857396cd98b72852529e5380c3b938fe1df39884
Dillon Birch <dillob26@gmail.com>
2026-02-05 19:10:02 -0700

    login jsx page

--- application/src/login/login.jsx
+++ application/src/login/login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+      <h1>Welcome to Word Chain</h1>
+        <p>Please login to play</p>
+        <form method="get" action="play.html">
+            <div>
+                <input type="text" placeholder="Username" className="form-control"/>
+            </div>
+            <div>
+                <input type="password" placeholder="Password" className="form-control"/>
+            </div>
+            <div className="account-buttons">
+                <button type="submit" className="btn btn-dark">Login</button>
+                <button type="submit" className="btn">Create</button>
+            </div>
+
+        </form>
     </main>
   );
 }
