34791af0269f00d0680366a99b25088e110b270f
Dillon Birch <dillob26@gmail.com>
2026-02-25 22:39:00 -0700

    restart changes the word chain

--- application/src/play/game.jsx
+++ application/src/play/game.jsx
@@ -4,15 +4,19 @@ import Button from 'react-bootstrap/Button';
 import { get_random_word } from './word_list';
 
 export function Game() {
-    const [startWord, setStartWord] = React.useState(get_random_word());
-    const [endWord, setEndWord] = React.useState(get_random_word());
+    const [start_word, set_start_word] = React.useState(get_random_word());
+    const [end_word, set_end_word] = React.useState(get_random_word());
 
+   function restart_game() {
+        set_start_word(get_random_word());
+        set_end_word(get_random_word());
+    }
 
     return (
         <div className="game">
             <div className="title">
                 <h3>Target Chain</h3>
-                <p><span id="start_word">{startWord}</span> -- <span id="end_word">{endWord}</span></p>
+                <p><span id="start_word">{start_word}</span> -- <span id="end_word">{end_word}</span></p>
             </div>
 
             <div className="letter-grid">
@@ -29,7 +33,7 @@ export function Game() {
 
             <div>
                 <button className="btn btn-dark">give up</button>
-                <button className="btn btn-dark">restart</button>
+                <button className="btn btn-dark" onClick={restart_game}>restart</button>
             </div>
         </div>);
 }
